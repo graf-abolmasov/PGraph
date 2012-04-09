7f55e8986e5b87ffa2174e854cc82a3a79864e80
apv1989 <apv1989@3a445295-52a1-4894-a4d6-5b639a95ca49>
2012-04-09 11:16:23 +0000

    fixed project

---
+++ RC/src/common/VO/graphvo.h
@@ -0,0 +1,90 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <QtCore/QList>
+#include <QtCore/QString>
+#include <QtGui/QFont>
+#include "../../src/common/VO/actor.h"
+#include "../../src/common/VO/predicate.h"
+#include "../../src/common/errors.h"
+
+class SyncArc
+{
+public:
+    SyncArc(QString startGraph, int startTop, QString endGraph, int endTop);
+    QString startGraph;
+    int startTop;
+    QString endGraph;
+    int endTop;
+    QStringList validate() const {return QStringList();}
+};
+
+class Comment
+{
+public:
+    Comment(float x, float y, QString text, QFont font);
+    float x;
+    float y;
+    QString text;
+    QFont font;
+};
+
+class Arc
+{
+public:
+    enum ArcType { SerialArc = 1, ParallelArc = 2, TerminateArc = 3};
+    Arc(ArcType type, int priority, int startTop, int endTop, const Predicate *predicate, const QStringList &lines);
+    ArcType type;
+    int priority;
+    int startTop;
+    int endTop;
+    QStringList lines;
+    const Predicate *predicate;
+
+    QStringList validate() const;
+};
+
+class Top
+{
+public:
+    enum TopType { NormalTop, MultiProcTop };
+    Top(float x, float y, float sizeX, float sizeY, int number, int procCount, bool isRoot, const Actor *actor, TopType type);
+    float x;
+    float y;
+    float sizeX;
+    float sizeY;
+    int number;
+    int procCount;
+    bool isRoot;
+    const Actor *actor;
+    TopType type;
+
+    QStringList validate() const;
+};
+
+class Project
+{
+public:
+    int id;
+    QString name;
+    QString description;
+    QString author;
+    Project(int id, QString name);
+};
+
+class Graph  : public Actor
+{
+public:
+    Graph(const QString &name, const QString &extName, const QList<Top> &topList, const QList<Arc> &arcList, const QList<Comment> &commentList, const QList<SyncArc> &syncArcList);
+    Graph(const QString &name, const QString &extName, const QList<Top> &topList, const QList<Arc> &arcList, const QList<Comment> &commentList, const QList<SyncArc> &syncArcList, const QPixmap &icon);
+    QList<Top>           topList;
+    QList<Arc>           arcList;
+    QList<Comment>       commentList;
+    QList<SyncArc>       syncArcList;
+
+    QList<Arc> getOutArcs(int topNumber) const;
+    int getRootTop() const;
+    QStringList validate() const;
+};
+
+#endif // GRAPH_H
