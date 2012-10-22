92e16d22a7e0b89cd8dc6996acbb13ab64162121
apv1989 <apv1989@3a445295-52a1-4894-a4d6-5b639a95ca49>
2012-10-22 17:45:10 +0000

    Updated Hanoi project

--- projects/Hanoi/C/SB1D636EA.cpp
+++
@@ -1,9 +0,0 @@
-#include "utype.h"
-#include "utype.h"PROJECT_BEGIN_NAMESPACE
-
-extern "C" int is_grater(TBar *left, TBar *right);
-int SB1D636EA(TBar *left, TBar *right)
-{
-	return is_grater(left, right);
-}
-PROJECT_END_NAMESPACE
