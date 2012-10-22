7bfec9e65401ee2310150696cc68b683d4426c1f
apv1989 <apv1989@3a445295-52a1-4894-a4d6-5b639a95ca49>
2012-10-22 17:45:10 +0000

    Updated Hanoi project

--- projects/Hanoi/C/S5F96A33C.cpp
+++
@@ -1,9 +0,0 @@
-#include "utype.h"
-PROJECT_BEGIN_NAMESPACE
-
-extern "C" int printf_bar(TBar *bar, TString *name);
-int S5F96A33C(TBar *bar, TString *name)
-{
-	return printf_bar(bar, name);
-}
-PROJECT_END_NAMESPACE
