cd33ae0f95e04b1d47196c022bd522c7fc0b9d5b
apv1989 <apv1989@3a445295-52a1-4894-a4d6-5b639a95ca49>
2013-08-26 20:02:42 +0000

    updated actor dialog

--- RC_Qt5/src/common/VO/actor.h
+++ RC_Qt5/src/common/VO/actor.h
@@ -16,9 +16,10 @@ public:
     enum Type {NormalType, InlineType, GraphType};
     Actor(const Actor &actor);
     Actor(const QString &name, const QString &extName, const Type &type);
-    Actor(const QString &name, const QString &extName, const Type &type, const BaseModule *baseModule, const QList<const Variable *> &variableList, const QStringList &varAccModeList, const QPixmap &icon);
+    Actor(const QString &name, const QString &extName, const Type &type, const BaseModule *baseModule, const QList<const Variable *> &variableList, const QStringList &varAccModeList, const QPixmap &icon, QString info);
     QString extName;
     QString name;
+    QString info;
     Type type;
     QStringList varAccModeList;
     const BaseModule *baseModule;
